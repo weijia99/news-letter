a231666f7361654a321f834b86e2fc664bff8932
weijia <646025053@qq.com>
2024-04-27 00:24:54 +0800

    init

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use actix_web::{web,App,HttpRequest,HttpServer,Responder};
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(||{
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
+
+async fn greet(req:HttpRequest) -> impl Responder{
+    let name = req.match_info().get("name").unwrap_or("world");
+    format!("Hello {}!",&name)
+}
