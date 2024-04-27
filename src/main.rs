efaf90c22ba1ed6529389c7574384f2703508bae
weijia <646025053@qq.com>
2024-04-27 11:04:47 +0800

    tokio explanation

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
-use actix_web::{web,App,HttpRequest,HttpServer,Responder};
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(||{
         App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
+            .route("/health_check", web::get().to(health_check))
     })
     .bind("127.0.0.1:8080")?
     .run()
     .await
 }
 
-async fn greet(req:HttpRequest) -> impl Responder{
-    let name = req.match_info().get("name").unwrap_or("world");
-    format!("Hello {}!",&name)
+async fn health_check() ->  HttpResponse{
+    HttpResponse::Ok().finish()
+}
+
+#[cfg(test)]
+mod test{
+    use crate::health_check;
+    #[tokio::test]
+    async fn health_check_succeeds(){
+        let response =health_check().await;
+        assert!(response.status().is_success());
+    }
 }
